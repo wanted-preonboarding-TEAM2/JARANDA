1f58e57beaedc99ca2702fe4a5aab08d2e72ffac
Hannah Park <ha3158987@gmail.com>
2021-08-02 21:41:38 +0900

    init: 초기 개발환경 세팅 (#2)
    - CRA설치
    - 절대경로설정
    - 폴더구조설정
    - Router설정

---
+++ src/constants/routesPath.js
@@ -0,0 +1,10 @@
+const ROUTES = {
+  HOME: '/',
+  ADMIN: '/admin',
+  TEACHER: '/teacher',
+  PARENTS: '/parents',
+  SIGNIN: '/signin',
+  SIGNUP: '/signup',
+};
+
+export default ROUTES;
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as Signin } from './Signin';
+export { default as Signup } from './Signup';
+export { default as Home } from './Home';
+export { default as Admin } from './Admin';
+export { default as Teacher } from './Teacher';
+export { default as Parents } from './Parents';
+export { default as RouteNoMatch } from './RouteNoMatch';
