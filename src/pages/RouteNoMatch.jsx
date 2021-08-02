faa35903e745ca4e8c4ad49dd78fac63c6e06f53
Hannah Park <ha3158987@gmail.com>
2021-08-02 21:41:38 +0900

    init: 초기 개발환경 세팅 (#2)
    - CRA설치
    - 절대경로설정
    - 폴더구조설정
    - Router설정

---
+++ src/pages/RouteNoMatch.jsx
@@ -0,0 +1,5 @@
+const RouteNoMatch = () => {
+      return <div>404 NOT FOUND</div>
+    };
+
+export default RouteNoMatch
---
+++ src/routes/routes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ROUTES from 'constants/routesPath';
+import * as Pages from 'pages'
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.HOME} component={Pages.Home} />
+        <Route path={ROUTES.ADMIN} component={Pages.Admin} />
+        <Route path={ROUTES.TEACHER} component={Pages.Teacher} />
+        <Route path={ROUTES.PARENTS} component={Pages.Parents} />
+        <Route path={ROUTES.SIGNIN} component={Pages.Signin} />
+        <Route path={ROUTES.SIGNUP} component={Pages.Signup} />
+        <Route component={Pages.RouteNoMatch} />
+      </Switch>
+    </Router>
+  )
+}
+
