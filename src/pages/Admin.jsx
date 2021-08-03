df7d75c543b33b6edc398b540a7331fb0526aa5f
mukjo96 <mukjo96@gmail.com>
2021-08-03 13:59:19 +0900

    feat: 테이블 컴포넌트 생성 (#14)

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,16 @@
 import SearchBox from 'components/Admin/SearchBox.jsx';
+import Table from 'components/Table';
 import React from 'react';
+import userMockData from 'res/mock/userMockData.json';
+
+const dataProps = ['id', 'name', 'address', 'card', 'age', 'role'];
 
 export default function Admin() {
   return (
     <div>
       Admin
       <SearchBox />
+      <Table dataProps={dataProps} tableData={userMockData} />
     </div>
   );
 }
