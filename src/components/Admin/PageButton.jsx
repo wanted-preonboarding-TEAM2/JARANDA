f918e8e21b94dd70b7ca507d9626bb56b8168cd0
Hannah Park <ha3158987@gmail.com>
2021-08-03 20:27:36 +0900

    feat: 페이지네이션 구현 (#23) - 페이지버튼 컴포넌트 생성

---
+++ src/components/Admin/PageButton.jsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+
+const PageButtonContainer = styled.li`
+
+`
+
+const StyledButton = styled.button`
+  border: 1px solid green;
+  width: 40px;
+  height: 40px;
+`
+
+function PageButton({ page, setCurrentPage, getCurrentPageUserInfos }) {
+  
+  const handleClickButton = () => {
+    setCurrentPage(page);
+    getCurrentPageUserInfos();
+  }
+  
+  return (
+    <PageButtonContainer>
+      <StyledButton onClick={handleClickButton}>
+        {page}
+      </StyledButton>
+    </PageButtonContainer>
+  )
+}
+
+export default PageButton;
