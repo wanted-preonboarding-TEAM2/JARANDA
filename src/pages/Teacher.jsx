80ee4e161297a87574544df44cb838865ceeb9e8
sanam <simian114@gmail.com>
2021-08-04 01:09:01 +0900

    chore: 페이지 내에서 현재 url 보이게 수정 (#35)

--- src/pages/Teacher.jsx
+++ src/pages/Teacher.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
 export default function Teacher() {
+  const match = useRouteMatch();
   return (
-    <div>
-      Teacher
-    </div>
-  )
+    <>
+      <div>Teacher Route!</div>
+      <div>{`currentURL: ${match.path}`}</div>
+    </>
+  );
 }
