beda798feca2ef8857a46b0ca3c3c5447cfccf50
Hannah Park <ha3158987@gmail.com>
2021-08-03 20:30:08 +0900

    feat: 페이지네이션 구현 (#23) - import PageList 컴포넌트

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,13 @@
-import SearchBox from 'components/Admin/SearchBox.jsx';
 import React from 'react';
+import SearchBox from 'components/admin/SearchBox.jsx';
+import PageList from 'components/admin/PageList.jsx';
 
 export default function Admin() {
   return (
     <div>
       Admin
       <SearchBox />
+      <PageList />
     </div>
   );
 }
