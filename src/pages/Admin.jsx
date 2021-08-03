761db348a040cf383f4cc8ed52350ac8b62fa157
mukjo96 <mukjo96@gmail.com>
2021-08-03 19:02:36 +0900

    feat: 데이터 테이블 구현 및 수정 (#14)

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,16 +1,34 @@
 import SearchBox from 'components/Admin/SearchBox.jsx';
-import Table from 'components/Table';
+import Table from 'components/Table/table';
 import React from 'react';
-import userMockData from 'res/mock/userMockData.json';
+import userMockData from 'components/Admin/users.json';
+import styled from '@emotion/styled';
+import TableHeader from 'components/Table/tableHeader';
 
 const dataProps = ['id', 'name', 'address', 'card', 'age', 'role'];
 
 export default function Admin() {
   return (
-    <div>
+    <div style={{ padding: '5%' }}>
       Admin
-      <SearchBox />
-      <Table dataProps={dataProps} tableData={userMockData} />
+      <TableContainer>
+        <HeaderContainer>
+          <TableHeader title="Users" number={1000} />
+          <SearchBox />
+        </HeaderContainer>
+        <Table dataProps={dataProps} tableData={userMockData} />
+      </TableContainer>
     </div>
   );
 }
+
+const TableContainer = styled.div`
+  background-color: #f8faff;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 24px;
+`;
