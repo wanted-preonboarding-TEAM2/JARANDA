7dbaf32e7babbb114d5bc3ef2f5939b2804c160e
mukjo96 <mukjo96@gmail.com>
2021-08-06 01:57:40 +0900

    feat: 404 에러 페이지 추가 (#95)

--- src/pages/RouteNoMatch.jsx
+++ src/pages/RouteNoMatch.jsx
@@ -1,5 +1,37 @@
-const RouteNoMatch = () => {
-      return <div>404 NOT FOUND</div>
-    };
+import styled from '@emotion/styled';
+import LinkButton from 'elements/LinkButton';
+import { MdError } from 'react-icons/md';
 
-export default RouteNoMatch
+const RouteNoMatch = () => {
+  return (
+    <Container>
+      <MdError />
+      <ErrorMessage>
+        <h3>404 Error</h3>
+        <h5>죄송합니다, 방문하신 페이지가 없습니다.</h5>
+      </ErrorMessage>
+      <StyledLinkButton to="/">홈으로 돌아가기</StyledLinkButton>
+    </Container>
+  );
+};
+
+export default RouteNoMatch;
+
+const Container = styled.div`
+  margin-top: 10vh;
+  font-size: 20vh;
+  text-align: center;
+  color: #aac14f;
+`;
+
+const ErrorMessage = styled.div`
+  font-size: 2vh;
+  color: #333;
+  h3 {
+    margin-bottom: 1vh;
+  }
+`;
+
+const StyledLinkButton = styled(LinkButton)`
+  margin: 5% 20%;
+`;
