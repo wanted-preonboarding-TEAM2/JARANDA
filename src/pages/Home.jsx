c278b963ef3cbed6abda4188f10c8901b6cb636e
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-08-05 11:00:51 +0900

    feat: H1 추가 후 숨김 (#68)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
 
 export default function Home() {
   return (
     <div>
-      Home 
+      Home
+      <HiddenStyledH1>
+        가장 퀄리티 있고 믿을 수 있는 선생님을 자란다에서만 만나보세요. 4-13세
+        아이에게 꼭 필요한 돌봄과 배움을 단짝샘과 함께. 국내 유일 1:1 플래너
+        관리. 국내 최고 선생님 퀄리티
+      </HiddenStyledH1>
     </div>
-  )
+  );
 }
+
+const HiddenStyledH1 = styled.h1`
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  width: 1px;
+  height: 1px;
+  white-space: nowrap;
+`;
