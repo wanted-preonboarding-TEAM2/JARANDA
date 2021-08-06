5500a4b55e27e00124b3d3fc488a4029b7ba19f5
yoonhe <yhehe228@gmail.com>
2021-08-06 11:39:57 +0900

    refactor: pages 폴더 구조 변경 - Admin ~ SignUp(#110)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,11 @@
-export { default as Signin } from './Signin';
-export { default as Signup } from './signup';
+export { default as Signin } from './SignIn/SignIn';
+export { default as Signup } from './SignUp/SignUp';
 
 export { default as Home } from './Home';
-export { default as Admin } from './Admin';
+export { default as Admin } from './Admin/Admin';
 export { default as Teacher } from './Teacher';
 export { default as Parents } from './Parents';
 export { default as RouteNoMatch } from './RouteNoMatch';
 
 export { default as Help } from './Help';
-export { default as ApplyTeacher } from './ApplyTeacher';
+export { default as ApplyTeacher } from './ApplyTeacher/ApplyTeacher';
