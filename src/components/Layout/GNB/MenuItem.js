70edbd45ed174e18916d22bc557b8a055d40a289
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-08-06 11:55:52 +0900

    fix: 로그아웃 GNB active 속성 fix (#112)

--- src/components/Layout/GNB/MenuItem.js
+++ src/components/Layout/GNB/MenuItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled';
 
-const MenuItemSytle = styled(NavLink)`
+const MenuItemStyle = styled(NavLink)`
   color: #999;
 
   & + & {
@@ -24,8 +24,12 @@ const MenuItemSytle = styled(NavLink)`
 
 export default function MenuItem({ children, to, handleLogout }) {
   return (
-    <MenuItemSytle to={to} activeStyle={{ color: '#333' }} onClick={handleLogout}>
+    <MenuItemStyle
+      to={to}
+      activeStyle={{ color: `${handleLogout ? 'color: #333' : 'color: #999'}` }}
+      onClick={handleLogout}
+    >
       <li>{children}</li>
-    </MenuItemSytle>
+    </MenuItemStyle>
   );
 }
